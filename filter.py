e38341572c57dbebd8fc97ebe58fc4ddeda85e67
Dineth-Mudalige <37803243+Dineth-Mudalige@users.noreply.github.com>
2023-10-27 14:32:41 +1100

    Add notch filter

--- filter.py
+++ filter.py
@@ -1,13 +1,20 @@
 import numpy as np
-from scipy.signal import butter, filtfilt
+from scipy.signal import butter, filtfilt, iirnotch, sosfreqz, sosfilt
 
 class DataFilter:
     def __init__(self, input_matrix):
         # Initialize  filter parameters here
         self.data = input_matrix
-        self.low_cutoff = 20
-        self.high_cutoff = 100
+        self.low_cutoff = 2
+        self.high_cutoff = 30
         self.sampling_rate = 1000
+        self.nyquist = 0.5 * self.sampling_rate
+        self.center_freq = 50
+
+    def apply_notch_filter(self, signal, Q=100):
+        freq = self.center_freq / self.nyquist
+        sos = iirnotch(freq, Q)
+        return sosfilt(sos, signal)
 
     def apply_filter(self, data,order=5):
         # Implement  data filtering logic here
@@ -24,5 +31,6 @@ class DataFilter:
         # Create an empty array of the same size
         empty_array = np.empty((num_rows, num_cols))
         for i in range(num_cols):
-            empty_array[:,i] = self.apply_filter(self.data[:,i])
+            filtered_data = self.apply_notch_filter(self.data[:,i])
+            empty_array[:,i] = self.apply_filter(filtered_data)
         return empty_array
